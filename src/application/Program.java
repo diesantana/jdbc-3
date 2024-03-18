1ce7c873fd0680524ed5a8581c686453e5ba4018
Diego Santana <3c.santana@gmail.com>
2024-03-18 08:58:17 -0300

    feat: inserindo um registro e retornando o valor do ID

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,9 @@ package application;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -20,24 +22,42 @@ public class Program {
 		try {
 			conn = DB.connectDb();
 			preparedSt = conn.prepareStatement("INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?)");
+					+ "VALUES (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
 			preparedSt.setString(1, "Walter White");
 			preparedSt.setString(2, "walter-white@gmail.com");
 
-			// LocalDate date = LocalDate.parse("18/03/1967", format);
-			// converte para a data SQL
-			// Date dateSQL = Date.valueOf(date);
+
 			preparedSt.setDate(3, Date.valueOf(LocalDate.parse("18/03/1967", format)));
 			preparedSt.setDouble(4, 3000.0);
 			preparedSt.setInt(5, 4);
 
 			int changedLines = preparedSt.executeUpdate();
+			
+			
+			if (changedLines > 0) {
+				try {
+				ResultSet result = preparedSt.getGeneratedKeys();
+				while(result.next()) {
+					int generatedId = result.getInt(1);	
+					System.out.println("Done! Id: " + generatedId);
+				}
+				} catch (Exception e) {
+					System.out.println("Error returning ID: " + e.getMessage());
+				}
+				
+				
+				System.out.println("Rows affected: " + changedLines);
+				
+				
+			} else {
+				System.out.println("No Rows affected!");
+			}
 
-			System.out.println("Done! Rows affected: " + changedLines);
 
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
+			
 		} finally {
 			DB.closeStatement(preparedSt);
 			DB.closeConnection();
